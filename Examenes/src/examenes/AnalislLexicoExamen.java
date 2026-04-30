4da6903d27896eb18fca7daa23e436d4e1a83882
YossHerrera <yh3075570Qgmail.com>
2026-04-30 08:16:45 -0600

    Commit 1

---
+++ Examenes/src/examenes/AnalislLexicoExamen.java
@@ -0,0 +1,16 @@
+package examenes;
+
+public class AnalislLexicoExamen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String entrada = "El lenguaje Java es excelente. HTML y CSS son tecnologías-web\n. " + 
+				"El valor de PI es=3.1416.Los números 42 y 0.5 son ejemplos.\n " + 
+				"HOLA,MUNDO. Una palabra como eStUdIo es válida.67-5.4.3\n " + 
+				"Esto es un texto de prueba. 3. .5 12a 51-50 holaMundo. A B C \n" + 
+				"MINUSCULAS minusculas MIXto mIxto 123 45.67 .78 99.";
+		//String regex =
+
+	}
+
+}
