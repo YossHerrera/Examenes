2fa5eafc98ae310609457b52d3d2df564fed88f4
YossHerrera <yh3075570Qgmail.com>
2026-04-29 19:55:52 -0600

    segunda prueba

--- Examenes/src/examenes/Pueba.java
+++ Examenes/src/examenes/Pueba.java
@@ -5,6 +5,7 @@ public class Pueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Prueba de Conexion con repositorio");
+		System.out.println("Prueba2 de Conexion con repositorio");
 
 	}
 
