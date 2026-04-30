07e586d55ae402c7e8c1fe09cda53de9ce83c816
YossHerrera <yh3075570Qgmail.com>
2026-04-30 08:31:18 -0600

    commit 8:30

--- Examenes/src/examenes/AnalislLexicoExamen.java
+++ Examenes/src/examenes/AnalislLexicoExamen.java
@@ -1,5 +1,9 @@
 package examenes;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AnalislLexicoExamen {
 
 	public static void main(String[] args) {
@@ -9,7 +13,18 @@ public class AnalislLexicoExamen {
 				"HOLA,MUNDO. Una palabra como eStUdIo es válida.67-5.4.3\n " + 
 				"Esto es un texto de prueba. 3. .5 12a 51-50 holaMundo. A B C \n" + 
 				"MINUSCULAS minusculas MIXto mIxto 123 45.67 .78 99.";
-		//String regex =
+		
+		String regex ="\\b//d+\\.//d+"
+				 Pattern pattern = Pattern.compile(regex);
+				 Matcher matcher = pattern.matcher(entrada);
+				
+			     ArrayList<String> numeros = new ArrayList<>();
+			     ArrayList<String> mixto = new ArrayList<>();
+			     ArrayList<String> soloMayusculas = new ArrayList<>();
+			     ArrayList<String> soloMinusculas = new ArrayList<>();
+			   
+			     while(matcher.)
+
 
 	}
 
